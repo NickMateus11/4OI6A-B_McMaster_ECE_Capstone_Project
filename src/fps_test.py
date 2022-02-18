0f00381cf22ea24fec844fb20cc25894fec15f2c
NickMateus11 <mateusn@mcmaster.ca>
2022-02-18 14:06:08 -0500

    pi camera FPS improvements and integration

--- src/fps_test.py
+++ src/fps_test.py
@@ -1,52 +1,50 @@
-# import the necessary packages
-from __future__ import print_function
-from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import argparse
-import imutils
-import time
-import numpy
-import cv2
-from maze_thread import MazeThread 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
-# initialize the camera and stream
-# created a *threaded *video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from `picamera` module...")
-vs = PiVideoStream().start()
-time.sleep(2.0)
-fps = FPS().start()
-maze_start = MazeThread().start()
-# TODO: maze_cmpr THREAD start 
-# loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read() 
-	maze = maze_start.read()
-	# TODO: store/read the maze_cmpr image into a variable in the main
-	
-	frame = imutils.resize(frame, width=400)
-	# check to see if the frame should be displayed to our screen
-	if args["display"] > 0:
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-	# update the FPS counter
-	fps.update()
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-print(maze_start.count)
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+# from imutils.video.pivideostream import PiVideoStream
+from pivideostream import PiVideoStream # custom library modification
+
+import argparse
+import time
+import cv2
+
+from maze_thread import MazeThread 
+
+
+def main(args):
+	# initialize the camera and stream
+	print("[INFO] sampling THREADED frames from `picamera` module...")
+	video_stream = PiVideoStream(resolution=(320, 240))
+	maze_stream = MazeThread(video_stream)
+
+	video_stream.start()
+	maze_stream.start()
+
+	start_time = time.time()
+	while time.time()-start_time < args["time"]:
+		# grab the frame from the threaded video stream
+		frame = video_stream.read() 
+		# frame = imutils.resize(frame, width=400)
+
+		# grab latest maze compression img
+		# TODO: event system? only grab when new frame ready
+		maze = maze_stream.read()
+
+		time.sleep(1)
+	
+	# stop the timer and display FPS information
+	video_stream.stop()
+	maze_stream.stop()
+
+	maze = cv2.resize(maze*255, (320, 240), interpolation=cv2.INTER_NEAREST)
+	cv2.imwrite("output.png", maze)
+
+	print(f"Maze fps:\t{maze_stream.count/(time.time()-start_time)}")
+	print(f"Video fps:\t{video_stream.frame_count/(time.time()-start_time)}")
+
+
+if __name__ == '__main__':
+	# construct the argument parse and parse the arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-t", "--time", type=int, default=5, help="# of frames to loop over for FPS test")
+	args = vars(parser.parse_args())
+
+	main(args)
--- src/maze_thread.py
+++ src/maze_thread.py
@@ -3,16 +3,20 @@ import cv2
 import numpy as np
 from image_test import maze_compression
 from threading import Thread
+from pivideostream import PiVideoStream
 
 class MazeThread:
-	def __init__(self):
+	def __init__(self, video_stream:PiVideoStream):
 		self.count = 0
 		self.x_grids = 8
 		self.y_grids = 8
 		self.wall_size = 4  # hard coded
-		self.sensitivity = 0.53
-		img_name = "./images/maze_ball_trim.png"
+		self.sensitivity = 0.85
+		self.video_stream = video_stream
+		img_name = "../images/maze_ball_trim.png"
 		self.img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)  # input
+		self.img = cv2.resize(self.img, (320, 240))
+		cv2.imwrite("imput.png", self.img)
 		self.maze = None
 		self.stopped = False
 
@@ -26,13 +30,17 @@ class MazeThread:
 	def update(self):
 		# keep looping infinitely until the thread is stopped
 		while (True):
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			new_maze, reference_maze = maze_compression(self.img, (self.y_grids, self.x_grids), self.wall_size, self.sensitivity,preprocess={'block': 255, 'blur':15, 'resize':5, 'adaptive':True})
-			self.maze = new_maze
-			self.count += 1
+			# grab the frame from the stream
+			# img = self.video_stream.read()
+			img = self.img
+			if img is not None:
+				# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+				new_maze, _ = maze_compression(img, (self.y_grids, self.x_grids), self.wall_size, 
+									self.sensitivity, preprocess={'thresh':30, 'blur':5, 'adaptive':False})
+				self.maze = new_maze
+				self.count += 1
+
 			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
 			if self.stopped:
 				return
 
