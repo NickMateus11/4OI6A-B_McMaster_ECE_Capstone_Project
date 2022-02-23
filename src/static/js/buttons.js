ef3eb0dc589906488479f41503b69e5b626e3540
NickMateus11 <mateusn@mcmaster.ca>
2022-02-23 18:25:03 -0500

    button cleanup

--- src/static/js/buttons.js
+++ src/static/js/buttons.js
@@ -1,20 +1,18 @@
 
 // Script to handle button POSTs
 
-$(function() {
-    $('#START').bind('click', function() {
-        $.getJSON('/print_hello',
-            function(data) {
-        //do nothing
-        });
-        return false;
+$('#START').bind('click', function() {
+    $.getJSON('/print_hello',
+        function(data) {
+    //do nothing
     });
+    return false;
+});
 
-    $('#STOP').bind('click', function() {
-        $.getJSON('/print_test',
-            function(data) {
-        //do nothing
-        });
-        return false;
+$('#STOP').bind('click', function() {
+    $.getJSON('/print_test',
+        function(data) {
+    //do nothing
     });
+    return false;
 });
