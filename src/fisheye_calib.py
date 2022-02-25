a625ce1b60f6acb0104e5905a27f7cd86c341e5b
NickMateus11 <mateus.nicholas@gmail.com>
2022-02-25 10:08:29 -0500

    fisheye calibration add undistort2

--- src/fisheye_calib.py
+++ src/fisheye_calib.py
@@ -61,10 +61,27 @@ def calib(images, internal_corners):
     return K, D, _img_shape[::-1]
 
 
-def undistort(img_path, K, D):
+def undistort(img_path, K, D, DIM):
     img = cv2.imread(img_path)
-    h,w = img.shape[:2]
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, (w,h), cv2.CV_16SC2)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    cv2.imshow("undistorted", undistorted_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def undistort2(img_path, K, D, DIM,balance=0.0, dim2=None, dim3=None):
+    img = cv2.imread(img_path)
+    dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
+    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+    if not dim2:
+        dim2 = dim1
+    if not dim3:
+        dim3 = dim1
+    scaled_K = K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
+    scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
+    # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. OpenCV document failed to make this clear!
+    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     cv2.imshow("undistorted", undistorted_img)
     cv2.waitKey(0)
@@ -72,5 +89,6 @@ def undistort(img_path, K, D):
 
 
 if __name__ == '__main__':
-    K, D, _ = calib(glob.glob("../images/calibration/pic_*.png"), CHECKERBOARD)
-    undistort("../images/calibration/distorted.png", K, D)
+    K, D, DIM = calib(glob.glob("../images/calibration/pic_*.png"), CHECKERBOARD)
+    undistort("../images/calibration/distorted.png", K, D, DIM)
+    undistort2("../images/calibration/distorted.png", K, D, DIM)
