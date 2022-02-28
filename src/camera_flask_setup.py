a88548a38929003face24f602acc167b2fa37f0e
NickMateus11 <mateusn@mcmaster.ca>
2022-02-28 15:49:52 -0500

    major updates to rendering

--- src/camera_flask_setup.py
+++ src/camera_flask_setup.py
@@ -1,26 +1,25 @@
-import cv2
 from imutils.video.pivideostream import PiVideoStream
-import imutils
+import cv2
 import time
 import numpy as np
 
 from fisheye_calib import undistort
-from image_utils import draw_grid
-
 
 class VideoCamera(object):
-    def __init__(self, resolution=(320,240), sensor_mode=0, flip=False):
+    def __init__(self, resolution=(320,240), sensor_mode=0, flip=False, correction=False, K=None, D=None):
         self.vs = PiVideoStream(resolution=resolution, 
                                 sensor_mode=sensor_mode)
         self.w, self.h = resolution
         self.flip = flip
+        self.latest_frame = None
 
         # Fisheye Params
-        # self.DIM=(320, 240)
-        # self.K=np.array([[154.88223395934025, 0.0, 152.79592302342448], [0.0, 154.70519413336302, 107.41300113111684], [0.0, 0.0, 1.0]])
-        # self.D=np.array([[-0.023124661972220774], [-0.01846311275378898], [-0.005734648752030648], [0.0012054217338599944]])
-        self.K=np.array([[154.59426972365793, 0.0, 153.6848583692119], [0.0, 154.3517630161616, 107.27567776876337], [0.0, 0.0, 1.0]])
-        self.D=np.array([[-0.012920290356728127], [-0.0811284577051761], [0.0956426843995683], [-0.04483463666625924]])
+        self.fisheye_correction = correction
+        if self.fisheye_correction:
+            if K is None:
+                self.K=np.array([[154.59426972365793, 0.0, 153.6848583692119], [0.0, 154.3517630161616, 107.27567776876337], [0.0, 0.0, 1.0]])
+            if D is None:
+                self.D=np.array([[-0.012920290356728127], [-0.0811284577051761], [0.0956426843995683], [-0.04483463666625924]])
         
         self.vs.start()
         time.sleep(2.0)
@@ -33,21 +32,18 @@ class VideoCamera(object):
     #         return np.flip(frame, 0)
     #     return frame
 
-    def get_frame(self, fisheye_correction=True, crop_dim=None):
+    def get_latest_frame(self, crop_region=None):
         frame = self.vs.read()
-
-            
-        if fisheye_correction:
+        
+        if self.fisheye_correction:
             frame = undistort(frame, self.K, self.D, (frame.shape[1], frame.shape[0]))
         
-        if crop_dim is None:
-            crop_dim = self.w, self.h
-        crop_amount_w = self.w - crop_dim[0] 
-        crop_amount_h = self.h - crop_dim[1]
-        frame = frame[0+crop_amount_h//2: self.h-crop_amount_h//2,
-                      0+crop_amount_w//2: self.w-crop_amount_w//2]
-
-        draw_grid(frame, 8,8)
+        if crop_region is not None:
+            crop_amount_w = self.w - crop_region[0] 
+            crop_amount_h = self.h - crop_region[1]
+            frame = frame[0+crop_amount_h//2: self.h-crop_amount_h//2,
+                          0+crop_amount_w//2: self.w-crop_amount_w//2]
         
-        return frame
+        self.latest_frame = frame
+        return self.latest_frame
 
--- src/camera_flask_test.py
+++ src/camera_flask_test.py
@@ -9,55 +9,64 @@ import pigpio
 import cv2
 
 from maze_thread import MazeThread 
+from image_utils import draw_grid
 
 
 SERVO_PIN_1 = 25
 SERVO_PIN_2 = 24
 
-# pwm = pigpio.pi()
-
-# pwm.set_mode(SERVO_PIN_1,pigpio.OUTPUT)
-# pwm.set_PWM_frequency(SERVO_PIN_1,50)
-
-# pwm.set_mode(SERVO_PIN_2,pigpio.OUTPUT)
-# pwm.set_PWM_frequency(SERVO_PIN_2,50)
-
-# Camera Setup
-mode = 0
-resolution = (320, 240)
-pi_camera = VideoCamera(resolution, sensor_mode=mode)
 
 # Flask App
 app = Flask(__name__, static_folder="static")
 
-def frame_gen(feed, kwargs={}):
+### Flask Routes ###
+@app.route('/')
+def index():
+    template_data = {
+        'v_width':pi_camera.w, 
+        'v_height':pi_camera.h
+        }
+    return render_template('index.html', **template_data)
+
+def frame_gen():
     #get camera frame
     while True:
-        frame = feed.get_frame(**kwargs)
+        frame = pi_camera.get_latest_frame()
+        h,w = frame.shape[:2]
+
+        crop_region = (240, 240)
+        crop_amount_w = w - crop_region[0] 
+        crop_amount_h = h - crop_region[1]
+
+        draw_grid(frame, 8,8, x_offset=crop_amount_w//2, y_offset=crop_amount_h//2)
+
+        frame = cv2.rectangle(
+            frame, 
+            (crop_amount_w//2+1, crop_amount_h//2+1), 
+            (w-crop_amount_w//2-1, h-crop_amount_h//2-1), 
+            (0,0,255), 2)
+
         _, jpeg = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
-### Flask Routes ###
 
-@app.route('/')
-def index():
-    data = {
-        'v_width':min(pi_camera.h, pi_camera.w), 
-        'v_height':min(pi_camera.h, pi_camera.w)
-        }
-    return render_template('index.html', **data) #you can customze index.html here
+def maze_gen():
+    #get maze frame
+    while True:
+        frame = maze_thread.get_maze_as_scaled_image()
+        _, jpeg = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+
 
 @app.route('/video_feed')
 def video_feed():
-    kwargs = {"fisheye_correction": True, "crop_dim":(240, 240)}
-    return Response(frame_gen(pi_camera, kwargs),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(frame_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/maze_feed')
 def maze_feed():
-    return Response(frame_gen(maze_thread),
-                mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(maze_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/print_hello')
 def print_hello():
@@ -73,8 +82,7 @@ def print_test():
 def capture():
     filename = "pic_temp.png"
     print(f"Saving Picture {filename}")
-    kwargs = {"fisheye_correction": True, "crop_dim":(240, 240)}
-    img = pi_camera.get_frame(**kwargs)
+    img = pi_camera.get_latest_frame()
     cv2.imwrite(filename, img)
     return send_file(filename, as_attachment=True)
 
@@ -108,9 +116,19 @@ def capture():
 
 if __name__ == '__main__':
 
+    # pwm = pigpio.pi()
+    # pwm.set_mode(SERVO_PIN_1,pigpio.OUTPUT)
+    # pwm.set_PWM_frequency(SERVO_PIN_1,50)
+    # pwm.set_mode(SERVO_PIN_2,pigpio.OUTPUT)
+    # pwm.set_PWM_frequency(SERVO_PIN_2,50)
+
+    # Camera Setup
+    mode = 0
+    resolution = (320, 240)
+    pi_camera = VideoCamera(resolution, sensor_mode=mode, correction=True) # in a thread
+
     # start maze thread
-    maze_thread = MazeThread(pi_camera)
-    maze_thread.start()
+    maze_thread = MazeThread(pi_camera) # in a thread
 
     # start flask server
     app.run(host='0.0.0.0', port=5000, threaded=True) #debug incompatible with resources available
