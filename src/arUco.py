d3a3860a71307c2882a4e855e03b639412d06440
NickMateus11 <mateus.nicholas@gmail.com>
2022-03-20 11:52:40 -0400

    minor change

--- src/arUco.py
+++ src/arUco.py
@@ -5,26 +5,29 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
 
-aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+# aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-fig = plt.figure()
-nx = 5
-ny = 10
-for i in range(1, nx*ny+1):
-    ax = fig.add_subplot(ny,nx, i)
-    img = aruco.drawMarker(aruco_dict,i-1, 700)
-    plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
-    ax.axis("off")
+# fig = plt.figure()
+# nx = 5
+# ny = 10
+# # for i in range(1, nx*ny+1):
+# #     ax = fig.add_subplot(ny,nx, i)
+# #     img = aruco.drawMarker(aruco_dict,i-1, 700)
+# #     plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
+# #     ax.axis("off")
 
-plt.show()
+# img = aruco.drawMarker(aruco_dict, 17, 700)
+# plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
+# plt.axis('off')
+# plt.show()
 
-frame = cv2.imread("../images/aruco.jpg")
+frame = cv2.imread("../images/maze_aruco.jpg")
 plt.figure()
 plt.imshow(frame)
 plt.show()
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters =  aruco.DetectorParameters_create()
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
